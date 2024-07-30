2cc8f49887511aa2d33bd8f89b0dbd943b2cb204
Nikhil <solankinikhil74@gmail.com>
2024-07-30 11:24:11 +0530

    newone

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Serve index.ejs at '/'
+
 app.get('/', (req, res) => {
   res.render('index');
 });
